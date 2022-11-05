7a8b2780004ec2f41d7e6360678c879f3b8725ce
AndrewRomanyshyn <morisszalivaet228@mail.ru>
2022-11-05 18:35:08 +0100

    Practicing with making class and attributes

---
+++ practice/taskSuperHero.py
@@ -0,0 +1,15 @@
+class SuperHero:
+    pass
+
+
+batman = SuperHero()
+superman = SuperHero()
+
+
+batman.can_fly = False
+batman.damage = 175
+
+
+superman.can_fly = True
+superman.damage = 285
+superman.alter_ego = "Кларк Кент"
